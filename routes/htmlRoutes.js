551bf97a750c269b063701b6769263ac22be6f41
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-05-03 14:50:16 -0400

    html changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,24 +1,20 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
-const express = require('express')
-const path = require('path');
-const root = { root: path.join(__dirname, "../public") };
+const express = require("express");
+const path = require("path");
 // ROUTING
 
 module.exports = (app) => {
-  app.use(express.static(path.join(__dirname, "../public")));
   // => HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
 
-  app.get('/notes', (req, res) => {
-    res.sendFile ('notes.html',root);
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
-  
-
   // If no matching route is found default to home
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // ROUTER
 // The below points our server to a series of "route" files.
